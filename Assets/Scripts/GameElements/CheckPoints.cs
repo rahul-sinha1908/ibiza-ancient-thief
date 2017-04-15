d251861193f3cffdd710979bf9941b22a7d63d9a
Rahul Sinha <rahul.sinha1908@gmail.com>
2017-04-15 14:22:25 +0530

    Made the base for thw game.

---
+++ Assets/Scripts/GameElements/CheckPoints.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPoints : MonoBehaviour {
+
+	public bool bCycle, bBullock, bHorse, bBoat;
+	public List<Transform> cycle, bullock, horse, boat;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
