1e480ff2c526efde7c67d0de2cb22ee54f16e0e2
Rahul Sinha <rahul.sinha1908@gmail.com>
2017-04-15 18:45:31 +0530

    Made the main menu basic structure.

---
+++ Assets/Scripts/GameScript/MainHandler.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainHandler : MonoBehaviour {
+
+	public Transform mainLoc, leftLoc, rightLoc;
+	public Transform mainMenu, searchMenu, lobby, waitMenu, eventSystem;
+	public Text waitText;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void createServer(){
+
+	}
+
+
+	public void searchForServer(){
+
+	}
+	
+	public void exit(){
+		Application.Quit();
+	}
+}
