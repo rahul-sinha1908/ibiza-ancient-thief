70bd19689f214075336c8362589d1f2dd48c3b91
Rahul Sinha <rahul.sinha1908@gmail.com>
2017-04-23 15:36:02 +0530

    made the uis for displaying the server datas.

--- Assets/Scripts/UIController/PoliceUIController.cs
+++ Assets/Scripts/UIController/PoliceUIController.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using MyGame;
 
 public class PoliceUIController : MonoBehaviour, SupremeController {
 	public List<PlayerControlScript> polices;
+	public GameObject displayMessage, listofMOves, movePrefab;
+	public Text displayText;
 	// Use this for initialization
 	void Start () {
 		
@@ -30,19 +33,27 @@ public class PoliceUIController : MonoBehaviour, SupremeController {
 	public void addThiefMoves(TransportType type){
 		//TODO Add the button in the scroll Pane.
 		Dev.log(Tag.PoliceUIController, "Added thief Button : "+type);
+		GameObject go=GameObject.Instantiate(movePrefab, Vector3.zero, Quaternion.identity, listofMOves.transform);
+		go.transform.FindChild("Text").GetComponent<Text>().text=""+type;
 	}
 
 	public void showTravellingWindow(){
 		Dev.log(Tag.PoliceUIController, "Showing the time left Window");
+		displayMessage.SetActive(true);
+		displayText.text="Travelling.. ";
 	}
 	public void showCooldownWindow(){
 		Dev.log(Tag.PoliceUIController, "Showing the Cooldowm Window");
+		displayMessage.SetActive(true);
+		displayText.text="Cool Down Time.. ";
 	}
 	public void hideAllWindows(){
 		Dev.log(Tag.PoliceUIController, "Removing all Windows");
+		displayMessage.SetActive(false);
 	}
 	public void setCooldownTime(int time){
-		
+		//displayMessage.SetActive(true);
+		displayText.text="Cool Down.. "+time+" sec";
 	}
 
 }
--- Assets/Scripts/UIController/ThiefUIController.cs
+++ Assets/Scripts/UIController/ThiefUIController.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using MyGame;
 
 public class ThiefUIController : MonoBehaviour, SupremeController {
 	public PlayerControlScript thief;
+	public GameObject displayMessage;
+	public Text displayText;
 	// Use this for initialization
 	void Start () {
 		
@@ -28,16 +31,21 @@ public class ThiefUIController : MonoBehaviour, SupremeController {
 	}
 
 	public void showTravellingWindow(){
-		Dev.log(Tag.ThiefUIController, "Showing the time left Window");
+		Dev.log(Tag.PoliceUIController, "Showing the time left Window");
+		displayMessage.SetActive(true);
+		displayText.text="Travelling.. ";
 	}
 	public void showCooldownWindow(){
-		Dev.log(Tag.ThiefUIController, "Showing the Cooldowm Window");
+		Dev.log(Tag.PoliceUIController, "Showing the Cooldowm Window");
+		displayMessage.SetActive(true);
+		displayText.text="Cool Down Time.. ";
 	}
 	public void hideAllWindows(){
-		Dev.log(Tag.ThiefUIController, "Removing all Windows");
+		Dev.log(Tag.PoliceUIController, "Removing all Windows");
+		displayMessage.SetActive(false);
 	}
 	public void setCooldownTime(int time){
-		
+		displayMessage.SetActive(true);
+		displayText.text="Cool Down.. "+time+" sec";
 	}
-
 }
